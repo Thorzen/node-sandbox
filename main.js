f63197e6e31e04b108d26ba5095d725d28dac8c0
Thorzen <jthorsen@outlook.com>
2021-02-19 14:09:41 -0500

    Now printing git log using exec (also in reverse)

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 #!/usr/bin/env node
 var shell = require('shelljs');
 
-shell.mkdir("test");
-shell.cd("test");
+//This is lazy, but it works.
+function reverseString(input) {
+    return input.split('').reverse().join('');
+}
+
+shell.mkdir('test');
+shell.cd('test');
+
+shell.touch('.gitlog');
+if (shell.which('git'))
+{
+    shell.exec('git log --oneline', { silent: true }, (code, stdout, stderr) => {
+        let lines = stdout.split('\n');
+        //Lines of git log reversed and in capital, because why not?
+        lines = lines.map(line => reverseString(line).toUpperCase());
+        shell.echo(lines.join('\n')).toEnd('.gitlog');
+
+        //Now print them back in the right order from, but read from the file.
+        let gitLines = shell.cat('.gitlog').split('\n');
+        gitLines = gitLines.map(reverseString);
+        shell.echo(gitLines.join('\n'));
+    });
+}
+
 
 shell.touch('foo.txt'); //Create foo.txt.
 shell.echo('this is a test').to('bar.txt'); //Write message to bar.txt.
@@ -13,4 +35,4 @@ shell.echo(files.toString());
 
 var dir = shell.pwd();
 shell.echo(dir.toString());
-shell.rm('-r', dir);
+process.on('exit', () => shell.rm('-r', dir));
