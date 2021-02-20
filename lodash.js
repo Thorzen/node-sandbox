f39611adf2b156be640e73406a355ac26f0cea36
Thorzen <jthorsen@outlook.com>
2021-02-20 12:31:47 -0500

    Added another way to check age assertion. This one just checks at end of sorted list.

--- lodash.js
+++ lodash.js
@@ -37,13 +37,19 @@ let meanOfAllAges = sortedUsersByBirthday.meanBy('age').value();
 console.log(`\nSum of all ages: ${sumOfAllAges}`);
 console.log(`\nMean of all ages: ${meanOfAllAges}`);
 
-//Is everyone above 14? Could use Array.every but that's not lodash and this is a lodash exercize!
-let ageCheck = youngest.every((user) => user.age > 14);
-console.log(`All users older than 14: ${ageCheck}`);
+//Is everyone above 14?
+const ageToCheck = 14;
+let ageCheck = youngest.every((user) => user.age > ageToCheck);
+console.log(`All users older than ${ageToCheck}: ${ageCheck}`);
 
 //Now the another way to do it, but this requires checking every value.
-ageCheck = youngest.minBy('age').get('age').value() > 14;
-console.log(`All users older than 14 (Method 2): ${ageCheck}`);
+ageCheck = youngest.minBy('age').get('age').value() > ageToCheck;
+console.log(`All users older than ${ageToCheck} (Method 2): ${ageCheck}`);
+
+//Another way is to just take the youngest user and check their age.
+//This should be the fastest since we are just checking one element.
+ageCheck = youngest.last().get('age').value() > ageToCheck;
+console.log(`All users older than ${ageToCheck} (Method 3): ${ageCheck}`);
 
 let [under30, overAnd30] = sortedUsersByBirthday.partition((user) => user.age < 30);
 console.log(`Ages under 30: ${under30.map(user => user.age)}`);
